10b100107710cda545f8f6d8c5b1ccbed2ae9cad
Tiimie1 <tim.pangersic@gmail.com>
2025-09-03 13:15:28 +0200

    feat(cppools): added cp pools card on home page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -10,7 +10,9 @@ const FeatureList = [
     Svg: require("@site/static/img/bc_web3_defactor.svg").default,
     description: (
       <>
-        Learn how to tokenize real-world assets with Defactor. Our guides cover everything from basics to advanced techniques, ensuring secure and transparent transactions.
+        Learn how to tokenize real-world assets with Defactor. Our guides cover
+        everything from basics to advanced techniques, ensuring secure and
+        transparent transactions.
       </>
     ),
   },
@@ -20,7 +22,9 @@ const FeatureList = [
     Svg: require("@site/static/img/sc_defactor.svg").default,
     description: (
       <>
-        Discover how to use Defactor's lending pools to unlock liquidity. Our resources will guide you through utilizing tokenized assets as collateral efficiently.
+        Discover how to use Defactor's lending pools to unlock liquidity. Our
+        resources will guide you through utilizing tokenized assets as
+        collateral efficiently.
       </>
     ),
   },
@@ -30,7 +34,21 @@ const FeatureList = [
     Svg: require("@site/static/img/api_defactor.svg").default,
     description: (
       <>
-        Engage with our community and governance tools on Defactor Engage. Find resources on network management, participation, and decision-making processes.
+        Engage with our community and governance tools on Defactor Engage. Find
+        resources on network management, participation, and decision-making
+        processes.
+      </>
+    ),
+  },
+  {
+    title: "Counterparty Pools",
+    link: "docs/category/cpPools/",
+    Svg: require("@site/static/img/cppools.svg").default,
+    description: (
+      <>
+        Explore Counterparty Pools. Provide collateral and set target APYs for
+        investors. Attract capital into your pool, reward investors with yields,
+        and maintain your position securely.
       </>
     ),
   },
@@ -38,14 +56,13 @@ const FeatureList = [
 
 const Feature = ({ Svg, title, description, link }) => {
   return (
-    <div className={clsx("col col--4")}>
+    <div className={clsx("col col--3")}>
       <div className={styles.featuresHeaderContainer}>
         <a href={link}>
           <div className={styles.featuresHeader}>
             <span className={styles.featuresSpam}>{title}</span>
-            <div className={styles.featureHoverHeader}>
-            </div>
-              <Svg className={styles.featureSvg} role="img" />
+            <div className={styles.featureHoverHeader}></div>
+            <Svg className={styles.featureSvg} role="img" />
           </div>
         </a>
       </div>
